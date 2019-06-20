441bc0936113284575f7d96bd59ce28029138898
Sanjay Y M <sanjayspear@gmail.com>
2019-06-21 01:06:28 +0530

    Free CRM updated with EventFiringWebdriver Listener and Extent Report

---
+++ FreeCRMTest/src/test/java/com/crm/qa/testcases/ContactsPageTest.java
@@ -0,0 +1,90 @@
+/*
+ * @author Naveen Khunteta
+ * 
+ */
+
+package com.crm.qa.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.ContactsPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
+
+public class ContactsPageTest extends TestBase{
+
+	LoginPage loginPage;
+	HomePage homePage;
+	TestUtil testUtil;
+	ContactsPage contactsPage;
+	
+	String sheetName = "contacts";
+	
+	   
+	public ContactsPageTest(){
+			super();
+			
+	}
+	
+	
+	@BeforeMethod
+	public void setUp() throws InterruptedException {
+		
+		initialization();
+		testUtil = new TestUtil();
+		contactsPage = new ContactsPage();
+		loginPage = new LoginPage();
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		TestUtil.runTimeInfo("error", "login successful");
+		testUtil.switchToFrame();
+		contactsPage = homePage.clickOnContactsLink();
+	}
+	
+	@Test(priority=1)
+	public void verifyContactsPageLabel(){
+		Assert.assertTrue(contactsPage.verifyContactsLabel(), "contacts label is missing on the page");
+	}
+	
+	@Test(priority=2)
+	public void selectSingleContactsTest(){
+		contactsPage.selectContactsByName("test2 test2");
+	}
+	
+	@Test(priority=3)
+	public void selectMultipleContactsTest(){
+		contactsPage.selectContactsByName("test2 test2");
+		contactsPage.selectContactsByName("ui uiii");
+
+	}
+	
+	@DataProvider
+	public Object[][] getCRMTestData(){
+		Object data[][] = TestUtil.getTestData(sheetName);
+		return data;
+	}
+	
+	
+	@Test(priority=4, dataProvider="getCRMTestData")
+	public void validateCreateNewContact(String title, String firstName, String lastName, String company){
+		homePage.clickOnNewContactLink();
+		//contactsPage.createNewContact("Mr.", "Tom", "Peter", "Google");
+		contactsPage.createNewContact(title, firstName, lastName, company);
+		
+	}
+	
+	
+
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+	
+	
+	
+	
+}
